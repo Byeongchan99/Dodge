9089b0a5123231e427b91c362e0815a07ccd8ba5
Byeongchan99 <harrrypoter@naver.com>
2024-06-04 03:28:02 +0900

    스테이지 선택 버튼 로직 정리

--- Assets/Scripts/UI/StageInfoButton.cs
+++ Assets/Scripts/UI/StageInfoButton.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UIManage;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,8 +10,25 @@ public class StageInfoButton : MonoBehaviour
     public UserData userData; // 임시 유저 데이터
     public StageInfoUI stageInfoUI;
 
+    // 필요한 매니저들
+    public StatDataManager statDataManager;
+    public TurretUpgradeHandler turretUpgradeHandler;
+    public PopupUIManager popupUIManager;
+    public StageManager stageManager;
+
+    // 매개변수들
+    public string statDataParameter;
+    public int turretUpgradeParameter;
+    public int setStageDataParameter;
+
     void Start()
     {
+        // 매니저들 동적 참조 설정
+        statDataManager = FindObjectOfType<StatDataManager>();
+        turretUpgradeHandler = FindObjectOfType<TurretUpgradeHandler>();
+        popupUIManager = FindObjectOfType<PopupUIManager>();
+        stageManager = FindObjectOfType<StageManager>();
+
         // Button 컴포넌트를 가져와서 클릭 이벤트에 리스너 추가
         Button button = GetComponent<Button>();
         if (button != null)
@@ -22,9 +40,30 @@ public class StageInfoButton : MonoBehaviour
     void OnButtonClick()
     {
         // 스테이지 정보를 업데이트
-        if (stageInfoUI != null && stageData != null && userData != null) 
+        if (stageInfoUI != null && stageData != null && userData != null)
         {
             stageInfoUI.UpdateStageInfo(stageData, userData);
+        }
+
+        // 각 매니저의 메서드 호출
+        if (statDataManager != null)
+        {
+            statDataManager.SetOriginalStatData(statDataParameter);
+        }
+
+        if (turretUpgradeHandler != null)
+        {
+            turretUpgradeHandler.SetStageEvents(turretUpgradeParameter);
+        }
+
+        if (popupUIManager != null)
+        {
+            popupUIManager.OpenStageInformationPopup();
+        }
+
+        if (stageManager != null)
+        {
+            stageManager.SetStageData(setStageDataParameter);
         }
     }
 }
