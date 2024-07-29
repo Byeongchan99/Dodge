c869af0522eea78c8ebba0094371bdeba608777c
Byeongchan99 <harrrypoter@naver.com>
2024-07-30 05:51:30 +0900

    터렛 정보 애니메이터 변경 로직

--- Assets/Scripts/UI/Popup/StageInfoUI.cs
+++ Assets/Scripts/UI/Popup/StageInfoUI.cs
@@ -8,7 +8,7 @@ public class StageInfoUI : MonoBehaviour, IUpdateUI
     public Text stageNameText;
     public Text stageInformationText;
     public Text highScore;
-    public Animator animator; // 애니메이터 컴포넌트
+    public RuntimeAnimatorController animatorController; // 애니메이터 컴포넌트
     public GameObject isGameClearImage;
     [SerializeField] private GameObject[] StarUnits; // 별 칸을 나타내는 GameObject 배열
 
@@ -16,7 +16,7 @@ public class StageInfoUI : MonoBehaviour, IUpdateUI
 
     void Start()
     {
-        animator = GetComponent<Animator>();
+        animatorController = GetComponent<RuntimeAnimatorController>();
     }
 
     public void UpdateUIInfo(params object[] datas)
@@ -29,7 +29,7 @@ public class StageInfoUI : MonoBehaviour, IUpdateUI
             if (data is StageData)
             {
                 stageData = data as StageData;
-                animator.runtimeAnimatorController = stageData.turretAnimatorController;
+                animatorController = stageData.turretAnimatorController;
             }
             else if (data is UserData)
             {
